b90d8d60237727251d58c4513e3b30f2a20ab4ec
DarshanMamtani <mamtanidarshan9@gmail.com>
2021-03-22 08:43:34 +0530

    Initialized Project, created basic frontend and basic express app

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// mongoose.connect('mongodb://localhost/shortUrl', {
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// });
+
+// Used to serve static files like HTML
+const static = express.static('public');
+// app.use() is Middlewear: Will be excuted before serving any request/end-points
+app.use(static);
+app.use(bodyParser.json);
+
+app.post("/shortUrl", (req, res) => {
+    console.log(req.body.url);
+    // await ShortUrl.create({
+    //     full: req.body.url
+    // })
+});
+
+app.listen(port, () => {
+    console.log(`Server Started at http://localhost:${port}`);
+});
