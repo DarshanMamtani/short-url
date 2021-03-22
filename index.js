8511b2a9c9b6301f4907b25d0b4117963a52453c
DarshanMamtani <mamtanidarshan9@gmail.com>
2021-03-22 09:03:10 +0530

    Connected to mongoDB with the help of mongoose

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// mongoose.connect('mongodb://localhost/shortUrl', {
-//     useNewUrlParser: true, 
-//     useUnifiedTopology: true
-// });
+mongoose.connect('mongodb://localhost/shortUrl', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
 
 // Used to serve static files like HTML
 const static = express.static('public');
@@ -16,11 +17,11 @@ const static = express.static('public');
 app.use(static);
 app.use(bodyParser.json());
 
-app.post("/shortUrl", (req, res) => {
+app.post("/shortUrl", async (req, res) => {
     console.log(req.body);
-    // await ShortUrl.create({
-    //     full: req.body.url
-    // })
+    await ShortUrl.create({
+        full: req.body.url
+    })
 });
 
 app.listen(port, () => {
