f9d4b343859db185850e331789f747ba400748fe
DarshanMamtani <mamtanidarshan9@gmail.com>
2021-03-24 12:55:47 +0530

    Almost project completed and working properly

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Connecting to Database || Arguments passed to avoid depricated warnings
 mongoose.connect('mongodb://localhost/shortUrl', {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+
 
 // Used to serve static files like HTML
 const static = express.static('public');
 // app.use() is Middlewear: Will be excuted before serving any request/end-points
 app.use(static);
-app.use(bodyParser.json());
+app.use(express.json());
+
 
 app.post("/shortUrl", async (req, res) => {
-    console.log(req.body);
     await ShortUrl.create({
         full: req.body.url
-    })
+    });
+    const shortUrls = await ShortUrl.find().sort({ $natural: -1 }).limit(5);
+    res.send(shortUrls);
+});
+
+app.get("/:shortUrl", async (req, res) => {
+    const doc = await ShortUrl.findOne({ short: req.params.shortUrl })
+
+    if(doc){
+        res.redirect(doc.full);
+    }
 });
 
 app.listen(port, () => {
